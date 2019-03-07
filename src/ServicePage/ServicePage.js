22485972f220f21d79e29007439dd938038cf712
silvanoamaral <silvano.amaral9@gmail.com>
2019-03-06 21:24:04 -0300

    Config framework Material Design

--- src/ServicePage/ServicePage.js
+++ src/ServicePage/ServicePage.js
@@ -8,13 +8,21 @@ export default class ServicePage extends Component {
                 <h3 className="sub__title">The standard Lorem Ipsum passage, used since the 1500s</h3>
                 <div className="row">
                     <div className="col">
-                    <p>One of three columns</p>
+                        <i className="material-icons dp48">settings</i>
+                        <h4>Rhoncus Purus In</h4>
+                        <p>One of three columns</p>
                     </div>
                     <div className="col">
-                    <p>One of three columns</p>
+                        <h4>Mattis Augue Tristique</h4>
+                        <p>One of three columns</p>
                     </div>
                     <div className="col">
-                    <p>One of three columns</p>
+                        <h4>Elementum Lacus Vitae</h4>
+                        <p>One of three columns</p>
+                    </div>
+                    <div className="col">
+                        <h4>Mauris Bibendum Odio Ligula</h4>
+                        <p>One of three columns</p>
                     </div>
                 </div>
             </article>
