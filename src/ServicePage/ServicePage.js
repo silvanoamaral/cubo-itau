5705afe9f9cc4f7f2a59a9de9f7e00dbc6f01d86
silvanoamaral <silvano.amaral9@gmail.com>
2019-03-11 22:02:20 -0300

    scss page service

--- src/ServicePage/ServicePage.js
+++ src/ServicePage/ServicePage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+
 export default class ServicePage extends Component {
 
     render() {
@@ -6,23 +7,30 @@ export default class ServicePage extends Component {
             <article className="container">
                 <h2 className="title">Serviços</h2>
                 <h3 className="sub__title">The standard Lorem Ipsum passage, used since the 1500s</h3>
-                <div className="row">
-                    <div className="col">
-                        <i className="material-icons dp48">settings</i>
-                        <h4>Rhoncus Purus In</h4>
-                        <p>One of three columns</p>
+                <div className="services">
+                    <div className="services_item">
+                        <div className="col">
+                            <i className="material-icons dp48">settings</i>
+                            <h4>Rhoncus Purus In</h4>
+                            <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</p>
+                        </div>
+                        <div className="col">
+                            <i className="material-icons dp48">settings</i>
+                            <h4>Mattis Augue Tristique</h4>
+                            <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</p>
+                        </div>
                     </div>
-                    <div className="col">
-                        <h4>Mattis Augue Tristique</h4>
-                        <p>One of three columns</p>
-                    </div>
-                    <div className="col">
-                        <h4>Elementum Lacus Vitae</h4>
-                        <p>One of three columns</p>
-                    </div>
-                    <div className="col">
-                        <h4>Mauris Bibendum Odio Ligula</h4>
-                        <p>One of three columns</p>
+                    <div className="services_item">                    
+                        <div className="col">
+                            <i className="material-icons dp48">settings</i>
+                            <h4>Elementum Lacus Vitae</h4>
+                            <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</p>
+                        </div>
+                        <div className="col">
+                            <i className="material-icons dp48">settings</i>
+                            <h4>Mauris Bibendum Odio Ligula</h4>
+                            <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</p>
+                        </div>
                     </div>
                 </div>
             </article>
