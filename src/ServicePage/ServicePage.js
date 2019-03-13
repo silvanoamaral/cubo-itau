fc1a3bbe417eec198509cac7f1d62626219dd353
silvano.amaral9@gmail.com <silvano.amaral9@gmail.com>
2019-03-13 17:32:13 -0300

    css page service

--- src/ServicePage/ServicePage.js
+++ src/ServicePage/ServicePage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+import './ServicePage.scss';
 export default class ServicePage extends Component {
 
     render() {
@@ -8,21 +9,21 @@ export default class ServicePage extends Component {
                 <h2 className="title">Serviços</h2>
                 <h3 className="sub__title">The standard Lorem Ipsum passage, used since the 1500s</h3>
                 <div className="services">
-                    <div className="services_item">
+                    <div className="services__item">
                         <div className="col">
                             <i className="material-icons dp48">settings</i>
                             <h4>Rhoncus Purus In</h4>
                             <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</p>
                         </div>
                         <div className="col">
-                            <i className="material-icons dp48">settings</i>
+                            <i className="material-icons dp48">card_giftcard</i>
                             <h4>Mattis Augue Tristique</h4>
                             <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</p>
                         </div>
                     </div>
-                    <div className="services_item">                    
+                    <div className="services__item">                    
                         <div className="col">
-                            <i className="material-icons dp48">settings</i>
+                            <i className="material-icons dp48">card_giftcard</i>
                             <h4>Elementum Lacus Vitae</h4>
                             <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</p>
                         </div>
